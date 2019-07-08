02489494b0e37d420b0a73bd78780b639c8d699e
Ryan Chase <ryan@chasery.com>
2019-07-08 09:34:24 -0700

    Add addTodo modal, method, and state

---
+++ client/src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = props => {
+    return ReactDOM.createPortal(
+        <div className="ui dimmer modals visible active">
+            <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    );
+}
+
+export default Modal;
--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Todo from './Todo';
 import { fetchTodos } from '../actions';
+import Todo from './Todo';
+import Modal from './Modal';
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { modalIsOpen: false, modalType: '' };
+    }
+
     componentDidMount() {
         this.props.fetchTodos();
     }
@@ -15,23 +22,36 @@ class TodoList extends React.Component {
         });
     }
 
+    renderAdd() {
+        return (
+            <div style={{ textAlign: 'right' }}>
+                <div className="ui animated fade button primary" tabIndex="0" onClick={this.addTodo}>
+                    <div className="visible content">Add Todo</div>
+                    <div className="hidden content">
+                        <i className="icon plus" style={{ margin: 0 }}></i>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    addTodo = () => {
+        this.setState(state => ({ modalIsOpen: !state.modalIsOpen, modalType: 'addTodo' }));
+    }
+
     render() {
         return (
             <div className="ui container">
-                <h2 className="ui header">React and Redux Todo List</h2>
+                <h2 className="ui header">React-Redux Todo List</h2>
                 <div className="ui divider"></div>
                 <div className="ui one cards">
                     {this.renderTodos()}
                 </div>
                 <div className="ui divider"></div>
-                <div style={{ textAlign: 'right' }}>
-                    <div className="ui animated fade button primary" tabIndex="0">
-                        <div className="visible content">Add Todo</div>
-                        <div className="hidden content">
-                            <i className="icon plus" style={{ margin: 0 }}></i>
-                        </div>
-                    </div>
-                </div>
+                {this.renderAdd()}
+                {this.state.modalIsOpen && this.state.modalType === 'addTodo' && 
+                    <Modal title="Add Todo" content="Add Todo Test" />
+                }
             </div>
         );
     }
